6fe069826963606ae39d64c2019a6b64f3c260bb
Jonathan Lloyd <jonathan@thisisjonathan.com>
2018-07-10 11:23:03 +0100

    Bump version to 0.10.5

--- tests.js
+++ tests.js
@@ -117,7 +117,7 @@ describe('PushNotifications Node SDK', () => {
                     'content-type': 'application/json',
                     'content-length': 55,
                     authorization: 'Bearer SECRET_KEY',
-                    'x-pusher-library': 'pusher-push-notifications-node 0.10.2',
+                    'x-pusher-library': 'pusher-push-notifications-node 0.10.5',
                     host: 'instance_id.pushnotifications.pusher.com'
                 });
                 expect(body).to.deep.equal({
