206e8998336fd3d7e90f604709129fbece7f2f43
minaorangina <mina@pusher.com>
2019-02-01 15:41:46 +0000

    Typings expect at least one platform in publish req body

--- index.d.ts
+++ index.d.ts
@@ -1,47 +1,46 @@
 declare module '@pusher/push-notifications-server' {
-    class PushNotifications {
-        constructor(options: PushNotifications.PushNotificationsOptions);
-        /**
-         *
-         * @param interests Array of interests to send the push notification to, ranging from 1 to 100 per publish request.
-         * @param publishRequest
-         */
-        publish<T extends PushNotifications.PublishRequestBase>(
-            interests: string[],
-            publishRequest: PushNotifications.PublishRequest
-        ): Promise<PushNotifications.PublishResponse>;
-    }
     namespace PushNotifications {
-        export interface PushNotificationsOptions {
+        interface Options {
             instanceId: string;
             secretKey: string;
             endpoint?: string;
         }
-        export interface PublishRequestBase {
+
+        interface PublishRequestBase {
             /* A URL to which we will send webhooks at key points throughout the publishing process. E.g when the publish finishes */
             webhookUrl?: string;
-            apns?: ApnsPayload;
-            fcm?: FcmPayload;
         }
-        export interface PublishRequestWithApns extends PublishRequestBase {
+
+        interface PublishRequestWithApns extends PublishRequestBase {
             apns: ApnsPayload;
         }
-        export interface PublishRequestWithFcm extends PublishRequestBase {
+
+        interface PublishRequestWithFcm extends PublishRequestBase {
             fcm: FcmPayload;
         }
-        export type PublishRequest =
+
+        interface PublishRequestWithApnsAndFcm extends PublishRequestBase {
+            apns: ApnsPayload,
+            fcm: FcmPayload
+        }
+
+        type PublishRequest =
+            | PublishRequestWithApns
             | PublishRequestWithFcm
-            | PublishRequestWithApns;
-        export interface FcmPayload {
+            | PublishRequestWithApnsAndFcm;
+
+        interface FcmPayload {
             /* This parameter specifies the predefined, user-visible key-value pairs of the notification payload */
             notification?: object;
             /* This parameter specifies the custom key-value pairs of the message's payload */
             data?: object;
         }
-        export interface ApnsPayload {
+
+        interface ApnsPayload {
             aps: ApsPayload;
         }
-        export interface ApsPayload {
+
+        interface ApsPayload {
             /**
              * Include this key when you want the system to display a standard alert or a banner. The notification settings
              * for your app on the user’s device determine whether an alert or banner is displayed.
@@ -65,7 +64,8 @@ declare module '@pusher/push-notifications-server' {
              */
             category?: string;
         }
-        export interface AlertPayload {
+
+        interface AlertPayload {
             /**
              * A short string describing the purpose of the notification. Apple Watch displays this string as part of the notification interface.
              * This string is displayed only briefly and should be crafted so that it can be understood quickly. This key was added in iOS 8.2.
@@ -74,10 +74,57 @@ declare module '@pusher/push-notifications-server' {
             /* The text of the alert message */
             body: string;
         }
-        export interface PublishResponse {
+
+        interface PublishResponse {
             /* Unique string used to identify this publish request */
             publishId: string;
         }
+
+        type Token = string;
     }
+
+    class PushNotifications {
+        constructor(options: PushNotifications.Options);
+        /**
+         * @param userId The given user id for which to generate a JWT token.
+         */
+        generateToken(userId: string): PushNotifications.Token;
+
+        /**
+         *
+         * @param interests Array of interests to send the push notification to, ranging from 1 to 100 per publish request.
+         * @param publishRequest
+         */
+        publish<T extends PushNotifications.PublishRequest>(
+            interests: string[],
+            publishRequest: T
+        ): Promise<PushNotifications.PublishResponse>;
+
+        /**
+         *
+         * @param interests Array of interests to send the push notification to, ranging from 1 to 100 per publish request.
+         * @param publishRequest
+         */
+        publishToInterests<T extends PushNotifications.PublishRequest>(
+            interests: string[],
+            publishRequest: PushNotifications.PublishRequest
+        ): Promise<PushNotifications.PublishResponse>;
+
+        /**
+         *
+         * @param users Array of user ids to send the push notification to, ranging from 1 to 1000 per publish request.
+         * @param publishRequest
+         */
+        publishToUsers<T extends PushNotifications.PublishRequest>(
+            users: string[],
+            publishRequest: PushNotifications.PublishRequest
+        ): Promise<PushNotifications.PublishResponse>;
+
+        /**
+         * @param userId The given user id to delete from the Pusher Beams service.
+         */
+        deleteUser(userId: string): Promise<void>;
+    }
+
     export = PushNotifications;
 }
