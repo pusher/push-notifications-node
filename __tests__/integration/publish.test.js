22e4b9aa008938ecfeafdadb0f145e6a74858253
minaorangina <mina@pusher.com>
2019-02-12 17:24:43 +0000

    adds test for delete users

--- __tests__/integration/publish.test.js
+++ __tests__/integration/publish.test.js
@@ -1,47 +1,57 @@
 const PushNotifications = require('../../push-notifications');
 
-describe('Publishes', () => {
-    const instanceId = '25c7b7c2-cabc-4e6b-b7d0-c9ad5c787e50';
-    const secretKey =
-        'A2DB402A49D7F7F45044D2049F5B2CDEB793564296B396A9B15A727ABE93EB50';
-    let beamsInstance;
+const instanceId = '25c7b7c2-cabc-4e6b-b7d0-c9ad5c787e50';
+const secretKey =
+    'A2DB402A49D7F7F45044D2049F5B2CDEB793564296B396A9B15A727ABE93EB50';
+let beamsInstance;
 
-    describe('Interests', () => {
-        beforeEach(() => {
-            beamsInstance = new PushNotifications({ instanceId, secretKey });
-        });
-        test('Publish request succeeds', () => {
-            const body = {
-                apns: {
-                    aps: {
-                        alert: 'Hello there'
-                    }
+describe('publishToInterests', () => {
+    beforeEach(() => {
+        beamsInstance = new PushNotifications({ instanceId, secretKey });
+    });
+    test('Publish request succeeds', () => {
+        const body = {
+            apns: {
+                aps: {
+                    alert: 'Hello there'
                 }
-            };
+            }
+        };
 
-            return expect(
-                beamsInstance.publishToInterests(['donuts'], body)
-            ).resolves.toHaveProperty('publishId', expect.any(String));
-        });
+        return expect(
+            beamsInstance.publishToInterests(['donuts'], body)
+        ).resolves.toHaveProperty('publishId', expect.any(String));
+    });
+});
+
+describe('publishToUsers', () => {
+    beforeEach(() => {
+        beamsInstance = new PushNotifications({ instanceId, secretKey });
     });
 
-    describe('Users', () => {
-        beforeEach(() => {
-            beamsInstance = new PushNotifications({ instanceId, secretKey });
-        });
-
-        test('Publish request succeeds', () => {
-            const body = {
-                apns: {
-                    aps: {
-                        alert: 'Hello there'
-                    }
+    test('Publish request succeeds', () => {
+        const body = {
+            apns: {
+                aps: {
+                    alert: 'Hello there'
                 }
-            };
+            }
+        };
 
-            return expect(
-                beamsInstance.publishToUsers(['user0001'], body)
-            ).resolves.toHaveProperty('publishId', expect.any(String));
-        });
+        return expect(
+            beamsInstance.publishToUsers(['user0001'], body)
+        ).resolves.toHaveProperty('publishId', expect.any(String));
+    });
+});
+
+describe('deleteUsers', () => {
+    beforeEach(() => {
+        beamsInstance = new PushNotifications({ instanceId, secretKey });
+    });
+
+    test('Deletion request succeeds', () => {
+        return expect(
+            beamsInstance.deleteUser('user0001')
+        ).resolves.toBeUndefined();
     });
 });
