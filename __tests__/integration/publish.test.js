7aba8800eff3b864be9ccee6eab9f809281421ff
minaorangina <mina@pusher.com>
2019-02-12 17:20:06 +0000

    integration tests use SDK

--- __tests__/integration/publish.test.js
+++ __tests__/integration/publish.test.js
@@ -1,93 +1,47 @@
-const request = require('request-promise-native');
+const PushNotifications = require('../../push-notifications');
 
 describe('Publishes', () => {
-    const SDK_VERSION = '1.1.1';
     const instanceId = '25c7b7c2-cabc-4e6b-b7d0-c9ad5c787e50';
     const secretKey =
         'A2DB402A49D7F7F45044D2049F5B2CDEB793564296B396A9B15A727ABE93EB50';
-    let baseUrl = `https://${instanceId}.pushnotifications.pusher.com/publish_api/v1/instances/${instanceId}/publishes`;
+    let beamsInstance;
 
     describe('Interests', () => {
-        let baseRequest;
-
         beforeEach(() => {
-            baseRequest = request.defaults({
-                headers: {
-                    Authorization: `Bearer ${secretKey}`,
-                    'X-Pusher-Library': `pusher-push-notifications-node ${SDK_VERSION}`,
-                    'Content-Type': 'application/json'
-                },
-                json: true,
-                simple: true,
-                resolveWithFullResponse: true
-            });
+            beamsInstance = new PushNotifications({ instanceId, secretKey });
         });
-        test('we get what we expect', () => {
+        test('Publish request succeeds', () => {
             const body = {
                 apns: {
                     aps: {
                         alert: 'Hello there'
                     }
-                },
-                interests: ['donuts']
-            };
-            const options = {
-                body,
-                uri: baseUrl + '/interests',
-                method: 'POST',
-                headers: {
-                    'Content-Length': Buffer.byteLength(JSON.stringify(body))
                 }
             };
 
-            return baseRequest(options).then(res => {
-                expect(res.body).toHaveProperty(
-                    'publishId',
-                    expect.any(String)
-                );
-            });
+            return expect(
+                beamsInstance.publishToInterests(['donuts'], body)
+            ).resolves.toHaveProperty('publishId', expect.any(String));
         });
     });
 
     describe('Users', () => {
-        let baseRequest;
-
         beforeEach(() => {
-            baseRequest = request.defaults({
-                headers: {
-                    Authorization: `Bearer ${secretKey}`,
-                    'X-Pusher-Library': `pusher-push-notifications-node ${SDK_VERSION}`,
-                    'Content-Type': 'application/json'
-                },
-                json: true,
-                simple: true,
-                resolveWithFullResponse: true
-            });
+            beamsInstance = new PushNotifications({ instanceId, secretKey });
         });
-        test('we get what we expect', () => {
+
+        test('Publish request succeeds', () => {
             const body = {
                 apns: {
                     aps: {
                         alert: 'Hello there'
                     }
-                },
-                users: ['user0001']
-            };
-            const options = {
-                body,
-                uri: baseUrl + '/users',
-                method: 'POST',
-                headers: {
-                    'Content-Length': Buffer.byteLength(JSON.stringify(body))
                 }
             };
 
-            return baseRequest(options).then(res => {
-                expect(res.body).toHaveProperty(
-                    'publishId',
-                    expect.any(String)
-                );
-            });
+            return expect(
+                beamsInstance.publishToUsers(['user0001'], body)
+            ).resolves.toHaveProperty('publishId', expect.any(String));
         });
     });
 });
