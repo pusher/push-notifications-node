dca3bb891b5cd7ee13001a1e639e2c14e4cf57de
Jonathan Lloyd <jonathan@thisisjonathan.com>
2019-04-23 13:44:39 +0100

    Bump version to 1.2.0

--- __tests__/publishToUsers.test.js
+++ __tests__/publishToUsers.test.js
@@ -57,7 +57,7 @@ describe('publishToUsers', () => {
                 'content-type': 'application/json',
                 'content-length': 72,
                 authorization: 'Bearer SECRET_KEY',
-                'x-pusher-library': 'pusher-push-notifications-node 1.1.1',
+                'x-pusher-library': 'pusher-push-notifications-node 1.2.0',
                 host: 'instance_id.pushnotifications.pusher.com'
             });
             expect(body).toEqual({
